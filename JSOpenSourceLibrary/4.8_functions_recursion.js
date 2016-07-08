51d2684fcb7c1d741199b9bc4658c0164e25f132
Paul <jamespauloconnor@gmail.com>
2016-07-08 07:31:54 -0400

    Bug fix.

--- JSOpenSourceLibrary/4.8_functions_recursion.js
+++ JSOpenSourceLibrary/4.8_functions_recursion.js
@@ -2,28 +2,15 @@
 
 //A recurisve function calls itself.
 
-var hanoi = function hanoi(disc, src, aux, dst){
-	if (disc> 0){
-		hanoi(disc-1, src, dst, aux);
-		console.log('Move disc ' + disc + ' from ' + src + ' to ' + dst);
-		hanoi(disc-1, aux, src, dst);
-	}
-};
 
-hanoi(1, 'Src', 'Aux', 'Dst');
-
-//currently
-//lh in 3rd
-//mb in 2nd
-//cow in 1st
-
-var countdown = function(value){
-	if (value > 0){
-		console.log(value);
-		return countdown(value-1);
+//This could be done with a while loop, but this is a demo.
+var timer = function(seconds){
+	if (seconds > 0){
+		console.log(seconds);
+		return timer(seconds-1);
 	}else{
-		return value;
+		return seconds;
 	}
 };
 
-countdown(10);
+timer(10);
