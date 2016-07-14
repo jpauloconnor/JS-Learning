6482402b9a3a5fd2cfd3296c4b345a9205ff7cb2
Paul <jamespauloconnor@gmail.com>
2016-07-14 13:52:07 -0400

    Switch statement added

---
+++ JSOpenSourceLibrary/1.51_switch.js
@@ -0,0 +1,15 @@
+var friend = "";
+
+switch (friend) {
+  case "Fred":
+    console.log("Hey Fred, let's go golfing.");
+    break;
+  case "Karl":
+    console.log("Let's hang.");
+    break;
+  case "John":
+    console.log("Sorry, I'm busy right now.");
+    break;
+  default:
+    console.log("Hey " + friend + "can I call you back in a minute.");
+}
