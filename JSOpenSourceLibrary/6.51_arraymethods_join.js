bca32301fa4762104ff0a8c4b6ffee946ad8983c
Paul <jamespauloconnor@gmail.com>
2016-07-23 11:44:01 -0400

    Added .join method

---
+++ JSOpenSourceLibrary/6.51_arraymethods_join.js
@@ -0,0 +1,16 @@
+
+
+var a = [1,2,3,4,5];
+//Print out the array as it is
+console.log(a);
+
+//THe join method makes a string from an array.
+//Concatenates them all togetherwith a separator
+//Example 1
+var c = a.join();
+console.log(c);
+
+
+//to take out the separtor, join with an empty string
+var d = a.join('');
+console.log(d);
