c9a32c0600d98c5a26d3935a6d3bbf308a0fae7f
Paul <jamespauloconnor@gmail.com>
2016-07-23 11:51:48 -0400

    Reverse method)

---
+++ JSOpenSourceLibrary/6.54_arraymethods_reverse.js
@@ -0,0 +1,18 @@
+
+var helloArray = ['h', 'e', 'l','l','o'];
+console.log(helloArray);
+
+//the reverse method will put the array in reverse order
+var reverseHelloArray = helloArray.reverse();
+console.log(reverseHelloArray);
+
+//Use the other methods that we've used to do other things
+var joinRevHello = reverseHelloArray.join('');
+console.log(joinRevHello);
+
+
+//MORE PRACTICE
+//Print name backwards...
+var name = ['P','a','u','l'];
+var revName = name.reverse().join('');
+console.log(revName);
