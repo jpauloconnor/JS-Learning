34234964168e3f2adf83f5ffa451a7776c4ee6ee
Paul <jamespauloconnor@gmail.com>
2016-07-24 21:55:03 -0400

    Shift method

---
+++ JSOpenSourceLibrary/6.57_arraymethods_shift.js
@@ -0,0 +1,15 @@
+//Shift removes teh first element from an array
+//returs it
+
+
+var a = [1, 2, 3, 4, 5, 6];
+console.log(a);
+
+var b = a.shift();
+console.log(b);
+
+//Now 1 is gone from the array.
+console.log(a);
+
+var c = a.shift();
+console.log(c);
