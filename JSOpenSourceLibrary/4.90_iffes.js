0bb66555bd2da6544a40f22f17f256bbbed95801
Paul O'Connor <jamespauloconnor@gmail.com>
2017-08-22 23:25:35 -0400

    Added some iffes

---
+++ JSOpenSourceLibrary/4.90_iffes.js
@@ -0,0 +1,53 @@
+//Example #1 - What is an IFFE?
+//IIFE - Immediately invoked function expression
+
+var myIIFEExampleWithUglyVariableName = (function(){
+	var aPrivateVar = "just some good ole private secrets that get exposed in the return only";
+	return {
+		sampleMethod: function(){
+			console.log( aPrivateVar );
+		}
+	};
+})();
+
+/* Notice that this breaks */
+	//console.log(aPrivateVar);
+
+//Notice that the data is expoed in the return...
+myIIFEExampleWithUglyVariableName.sampleMethod();
+
+
+//Example #2
+//creating private data
+var bass = (function() {
+
+    // private
+    var quarterNote = 'ta ';
+    var eigthNote = 'ta ta';
+    var sixteenthNote = 'ta ca di me';
+    
+    return {
+        slogan: "Let's have a thump off!",
+
+        playQuarterNotes: function() {
+            console.log( quarterNote );
+        },
+
+        playEigthNotes: function() {
+            console.log( eigthNote );
+        },
+
+        playSixteenthNotes: function(){
+        	console.log( sixteenthNote );
+        }
+    };
+})();
+
+//console.log(quarterNote);
+bass.playQuarterNotes();
+bass.playEigthNotes();
+bass.playSixteenthNotes();
+
+
+
+
--- JSOpenSourceLibrary/7.0_regexpressions.js
+++ JSOpenSourceLibrary/7.0_regexpressions.js
@@ -1,12 +1,6 @@
-//Must use strict mode to be able to use classes.
-"use strict";
-//As of ECM6, we can now make traditional functions
-class Player {
-	constructor(name, number){
-		this.name = name,
-		this.number = number
-	}
-}
+//Regular Expressions
+//Used with methods to search, replace, and extract info from strings.
 
-var andrewLuck = new Player("Andrew Luck", 19);
-console.log(andrewLuck);
+var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+
+var url = ""
